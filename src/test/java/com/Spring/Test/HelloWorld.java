fea31258810c88a3055e9f656c2e61595fcd4b19
zhangrui <lifeway8140@gmail.com>
2018-03-23 17:25:01 +0800

    初步搭建Maven Spring

---
+++ src/test/java/com/Spring/Test/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.Spring.Test;
+
+public class HelloWorld {
+    private String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void printHello() {
+        System.out.println("Spring 3 : Hello ! " + name);
+    }
+}
---
+++ src/test/java/com/Spring/Test/Main.java
@@ -0,0 +1,13 @@
+package com.Spring.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext(
+                "SpringBeans.xml");
+
+        HelloWorld obj = (HelloWorld) context.getBean("helloBean");
+        obj.printHello();
+    }
+}
