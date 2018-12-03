bbcbdd1fdfd654f6603763a9d4375833acb2b171
zhangrui <lifeway8140@gmail.com>
2018-12-03 17:47:30 +0800

    Controller

--- src/main/java/business/controller/HelloWorldController.java
+++ src/main/java/business/controller/HelloWorldController.java
@@ -3,15 +3,37 @@ package business.controller;
 import core.basic.BaseController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /*@Controller
 @RequestMapping("/hello")*/
-public class HelloWorldController extends BaseController {
+@SessionAttributes("message")
+public class HelloWorldController extends AbstractController {
+
     @Override
+    protected ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        //1、收集参数
+        //2、绑定参数到命令对象
+        //3、调用业务对象
+        //4、选择下一个页面
+        ModelAndView mv = new ModelAndView();
+        //添加模型数据 可以是任意的POJO对象
+        mv.addObject("message", "Hello World!");
+        //设置逻辑视图名，视图解析器会根据该名字解析到具体的视图页面
+        mv.setViewName("HelloWorld");
+        return mv;
+    }
+
+
+
+
+   /* @Override extends Controller
     public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
         //1、收集参数、验证参数
         //2、绑定参数到命令对象
@@ -23,5 +45,5 @@ public class HelloWorldController extends BaseController {
         //设置逻辑视图名，视图解析器会根据该名字解析到具体的视图页面
         mv.setViewName("HelloWorld");
         return mv;
-    }
+    }*/
 }
--- src/main/java/business/service/IHelloWorldService.java
+++
@@ -1,6 +0,0 @@
-package business.service;
-import business.bean.HelloWorldBean;
-
-public interface IHelloWorldService {
-   HelloWorldBean getHelloWorldBean(String id);
-}
