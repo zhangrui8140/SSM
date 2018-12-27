549e6a3f1f40cb2b5871e7c01f3beeeecc0856b4
zhangrui <lifeway8140@gmail.com>
2018-12-27 15:13:06 +0800

    Title:Beans SPEL Author:zhangrui

---
+++ src/test/java/springsourcetest/SpringSourceTest.java
@@ -0,0 +1,32 @@
+package springsourcetest;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.expression.Expression;
+import org.springframework.expression.ExpressionParser;
+import org.springframework.expression.ParserContext;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
+
+public class SpringSourceTest {
+    @Test
+    public void testParserContext() {
+        ExpressionParser parser = new SpelExpressionParser();
+        ParserContext parserContext = new ParserContext() {
+            @Override
+            public boolean isTemplate() {
+                return true;
+            }
+            @Override
+            public String getExpressionPrefix() {
+                return "#{";
+            }
+            @Override
+            public String getExpressionSuffix() {
+                return "}";
+            }
+        };
+        String template = "#{'Hello '}#{'World!'}";
+        Expression expression = parser.parseExpression(template, parserContext);
+        Assert.assertEquals("Hello World!", expression.getValue());
+    }
+}
